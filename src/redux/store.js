41bc182d34e9c8aa7be05ee600c3745ca4f413be
Oleksandr Lutsenko <sashalutsenko1991@gmail.com>
2024-03-23 01:49:09 +0100

    d

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import contactsReducer from './contactsSlice';
-import filtersReducer from './filtersSlice';
+import filtersReducer from './filterSlice';
 import storage from 'redux-persist/lib/storage';
 import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 
